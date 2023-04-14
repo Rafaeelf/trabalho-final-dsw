a241c95b4b26ccb6f6baecc9ca7476d206be9cbf
Rafaeelf <rafael.fossa@hotmail.com>
2023-04-14 20:56:46 -0300

    Update detalhes do produto

--- src/Componentes/Card.js
+++ src/Componentes/Card.js
@@ -1,4 +1,5 @@
 import './Card.css';
+import { Link } from "react-router-dom";
 
 export default function Card(){
 
@@ -11,10 +12,24 @@ export default function Card(){
           <div className="card">
              <img class="card-img-top" src = "./img/camisaTeste.png"></img>
           
-            <div class="card-body">
+                  <div class="card-body">
                     <h5>Camisa Vermelha Estampada</h5>
                     <p class="card-text"> R$ 50 pila </p>
-                    <a href="#" class="btn btn-primary">Visitar</a>
+                    <Link className="nav-link" to="/produtoDetalhes">
+                        <a href="#" class="btn btn-primary">Visitar</a>
+                    </Link>
+            </div>
+        </div>
+          </div>
+          <div class="col">
+          <div className="card">
+             <img class="card-img-top" src = "./img/camisaTeste.png"></img>
+          
+            <div class="card-body">
+                    <h5>Camisa Vermelha Estampada</h5>
+                    <Link className="nav-link" to="/produtoDetalhes">
+                        <a href="#" class="btn btn-primary">Visitar</a>
+                    </Link>
             </div>
         </div>
           </div>
@@ -25,29 +40,9 @@ export default function Card(){
             <div class="card-body">
                     <h5>Camisa Vermelha Estampada</h5>
                     <p class="card-text"> R$ 50 pila </p>
-                    <a href="#" class="btn btn-primary">Visitar</a>
-            </div>
-        </div>
-          </div>
-          <div class="col">
-          <div className="card">
-             <img class="card-img-top" src = "./img/camisaTeste.png"></img>
-          
-            <div class="card-body">
-                    <h5>Camisa Vermelha Estampada</h5>
-                    <p class="card-text"> R$ 50 pila </p>
-                    <a href="#" class="btn btn-primary">Visitar</a>
-            </div>
-        </div>
-          </div>
-          <div class="col">
-          <div className="card">
-             <img class="card-img-top" src = "./img/camisaTeste.png"></img>
-          
-            <div class="card-body">
-                    <h5>Camisa Vermelha Estampada</h5>
-                    <p class="card-text"> R$ 50 pila </p>
-                    <a href="#" class="btn btn-primary">Visitar</a>
+                    <Link className="nav-link" to="/produtoDetalhes">
+                        <a href="#" class="btn btn-primary">Visitar</a>
+                    </Link>
             </div>
         </div>
           </div>
--- src/Componentes/ProdutoDetalhes.js
+++ src/Componentes/ProdutoDetalhes.js
@@ -1,7 +1,12 @@
 
-
+import './ProdutoDetalhes.css';
 
 export default function ProdutoDetalhes() {
 
+    return(
+
+        <h1>teste</h1>
+
+    );
 
 }
