7153fbeb084d84c9a261466a7e460295307c0850
Rafaeelf <rafael.fossa@hotmail.com>
2023-05-02 20:15:10 -0300

    ....

--- src/Componentes/Cabecalho.js
+++ src/Componentes/Cabecalho.js
@@ -1,68 +1,79 @@
 import { Link } from "react-router-dom";
-import React from 'react';
-import './Cabecalho.css';
+import React from "react";
+import "./Cabecalho.css";
 import { AutCtx, useAutCtx } from "../autCtx";
 
-function Cabecalho(){
+function Cabecalho() {
+  const autCtx = useAutCtx();
+  const foiAutenticado = autCtx.autenticado;
 
-    const autCtx = useAutCtx();
-    const foiAutenticado = autCtx.autenticado;
+  function sair() {
+    autCtx.sair();
+  }
 
-    function sair(){
-        autCtx.sair();
-    }
-
-    return (
-        <header className="border-bottom border-light border-5 mb-5 p-2">
-            <div className="container">
-                <div className="row">
-                <nav class="navbar navbar-expand-lg navbar-light bg-light">	
-                <img alt="logo" src="./img/logo.png" ></img>	 
-                    <a class="navbar-brand" href="#">DroneSeta</a>
-                    <div className="collapse navbar-collapse">
-                        <ul className="navbar-nav">
-                            <li className="nav=item">
-                                <Link className="nav-link" to="/">                                    
-                                   <a>Home</a>
-                                </Link>
-                            </li>
-                            <li className="nav=item">
-                                <Link className="nav-link" to = "/produtos">
-                                   <a>Produtos</a>
-                                </Link>
-                            </li>
-                            <li className="nav=item">
-                                <Link className="nav-link" to = "/produtos/cadastro">
-                                   <a>Cadastrar Produtos</a>
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
-                    <div>
-                        <ul class="navbar-nav justify-content-end">
-                            <li class="nav-item">
-                                {!foiAutenticado &&
-                                <Link className="nav-link" to="/login">Efetuar Login</Link>}
-                            </li>
-                            <li class="nav-item">
-                                {!foiAutenticado &&
-                                <Link className="nav-link" to="/login">Efetuar Login</Link>}
-                            </li>
-                            <li className="nav=item">
-                                {foiAutenticado &&
-                                <Link className="nav-link" to="/inicio" onClick={sair}>Sair</Link>}
-                            </li>
-                            <li class="nav-item">
-                                {foiAutenticado &&
-                                <Link className="nav-link" to = "/carrinho">Carrinho</Link>}
-                            </li>
-                        </ul>
-                    </div>
-                  </nav>
-                </div>
-            </div>
-        </header>
-    )
+  return (
+    <div className="container">
+      <div className="row">
+        <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+          <img alt="logo" src="./img/logo.png"></img>
+          <a class="navbar-brand" href="#">
+            DroneSeta
+          </a>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav">
+              <li className="nav=item">
+                <Link className="nav-link" to="/">
+                  <a>Home</a>
+                </Link>
+              </li>
+              <li className="nav=item">
+                <Link className="nav-link" to="/produtos">
+                  <a>Produtos</a>
+                </Link>
+              </li>
+              <li className="nav=item">
+                <Link className="nav-link" to="/produtos/cadastro">
+                  <a>Cadastrar Produtos</a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <ul class="navbar-nav justify-content-end">
+              <li class="nav-item">
+                {!foiAutenticado && (
+                  <Link className="nav-link" to="/login">
+                    Efetuar Login
+                  </Link>
+                )}
+              </li>
+              <li class="nav-item">
+                {!foiAutenticado && (
+                  <Link className="nav-link" to="/login">
+                    Efetuar Login
+                  </Link>
+                )}
+              </li>
+              <li className="nav=item">
+                {foiAutenticado && (
+                  <Link className="nav-link" to="/inicio" onClick={sair}>
+                    Sair
+                  </Link>
+                )}
+              </li>
+              <li class="nav-item">
+                {foiAutenticado && (
+                  <Link className="nav-link" to="/carrinho">
+                    Carrinho
+                  </Link>
+                )}
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
 }
 
 export default Cabecalho;
